6a9b76f9e95ea150bdc2f3587ce8dffe258414f0
sekibaalamark <markmajje256@gmail.com>
2026-08-06 21:13:11 +0300

    Project set up

---
+++ src/main/java/com/cit/student_api_v2/StudentApiV2Application.java
@@ -0,0 +1,13 @@
+package com.cit.student_api_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApiV2Application.class, args);
+	}
+
+}
