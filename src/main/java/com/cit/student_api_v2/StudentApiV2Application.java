c40058d20cbf1d97078e8bd26bf2eb740cda204e
sekibaalamark <markmajje256@gmail.com>
2026-08-07 00:14:25 +0300

    main class

--- src/main/java/com/cit/student_api_v2/StudentApiV2Application.java
+++ src/main/java/com/cit/student_api_v2/StudentApiV2Application.java
@@ -2,6 +2,7 @@ package com.cit.student_api_v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class StudentApiV2Application {
