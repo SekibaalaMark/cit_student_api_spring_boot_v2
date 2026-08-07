79f7112286acd7cd47a11e1bc06cd3d18eca277d
sekibaalamark <markmajje256@gmail.com>
2026-08-07 11:02:35 +0300

    created a student mapper between student request and entity

---
+++ src/main/java/com/cit/student_api_v2/student/mapper/StudentMapper.java
@@ -0,0 +1,43 @@
+package com.cit.student_api_v2.student.mapper;
+
+
+import com.cit.student_api_v2.student.dto.StudentRequest;
+import com.cit.student_api_v2.student.dto.StudentUpdateRequest;
+import com.cit.student_api_v2.student.model.Student;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentMapper {
+
+    public Student toEntity(StudentRequest studentRequest){
+        if(studentRequest == null){
+            return null;
+        }
+
+        Student student = new Student();
+        student.setName(studentRequest.getName());
+        student.setRegistrationNumber(studentRequest.getRegistrationNumber());
+        student.setCgpa(studentRequest.getCgpa());
+        return student;
+    }
+
+
+    public void updateEntityFromRequest(StudentUpdateRequest studentRequest, Student existingStudent){
+        if(studentRequest == null || existingStudent == null){
+            return;
+        }
+
+        if(studentRequest.getName() != null){
+            existingStudent.setName(studentRequest.getName());
+        }
+
+        if(studentRequest.getRegistrationNumber() != null){
+            existingStudent.setRegistrationNumber(studentRequest.getRegistrationNumber());
+        }
+
+        if(studentRequest.getCgpa() != null){
+            existingStudent.setCgpa(studentRequest.getCgpa());
+        }
+    }
+
+}
