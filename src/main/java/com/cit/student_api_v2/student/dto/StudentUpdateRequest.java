84b6f3e321b0f57364d96b60827350dea3c28c5a
sekibaalamark <markmajje256@gmail.com>
2026-08-07 11:05:28 +0300

    created a student update dto

---
+++ src/main/java/com/cit/student_api_v2/student/dto/StudentUpdateRequest.java
@@ -0,0 +1,38 @@
+package com.cit.student_api_v2.student.dto;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+
+public class StudentUpdateRequest {
+    private String name;
+    private String registrationNumber;
+
+    @DecimalMin(value = "0.0", message = "GPA must be at least 0.0")
+    @DecimalMax(value = "5.0", message = "GPA cannot exceed 5.0")
+    private Double cgpa;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public Double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(Double cgpa) {
+        this.cgpa = cgpa;
+    }
+}
