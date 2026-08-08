afbd551db02c2180475f248ade3a53ff6834b2ef
sekibaalamark <markmajje256@gmail.com>
2026-08-08 10:11:13 +0300

    Add method to map student entity to clean response

--- src/main/java/com/cit/student_api_v2/student/mapper/StudentMapper.java
+++ src/main/java/com/cit/student_api_v2/student/mapper/StudentMapper.java
@@ -2,6 +2,7 @@ package com.cit.student_api_v2.student.mapper;
 
 
 import com.cit.student_api_v2.student.dto.StudentRequest;
+import com.cit.student_api_v2.student.dto.StudentResponse;
 import com.cit.student_api_v2.student.dto.StudentUpdateRequest;
 import com.cit.student_api_v2.student.model.Student;
 import org.springframework.stereotype.Component;
@@ -38,6 +39,17 @@ public class StudentMapper {
         if(studentRequest.getCgpa() != null){
             existingStudent.setCgpa(studentRequest.getCgpa());
         }
+    }
+
+    public StudentResponse toResponse(Student student){
+        if(student == null){
+            return null;
+        }
+        StudentResponse response = new StudentResponse();
+        response.setCgpa(student.getCgpa());
+        response.setName(student.getName());
+        response.setRegistrationNumber(student.getRegistrationNumber());
+        return response;
     }
 
 }
