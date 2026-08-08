26f677fde2b607365c7557490f1782f19bfdbb4b
sekibaalamark <markmajje256@gmail.com>
2026-08-08 10:10:35 +0300

    Add ApiResponse on the Get all students Controller

--- src/main/java/com/cit/student_api_v2/student/controller/StudentController.java
+++ src/main/java/com/cit/student_api_v2/student/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.cit.student_api_v2.student.controller;
+import com.cit.student_api_v2.api.response.ApiResponse;
 import com.cit.student_api_v2.student.dto.StudentRequest;
+import com.cit.student_api_v2.student.dto.StudentResponse;
 import com.cit.student_api_v2.student.dto.StudentUpdateRequest;
 import com.cit.student_api_v2.student.model.Student;
 import com.cit.student_api_v2.student.service.StudentService;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,13 +22,20 @@ public class StudentController {
     }
 
     @GetMapping
-    public List<Student> getAllStudents(){
-        return studentService.findAll();
+    public ResponseEntity<ApiResponse<List<StudentResponse>>>  getAllStudents(){
+        List<StudentResponse> data=  studentService.findAll();
+        ApiResponse<List<StudentResponse>> apiResponse = new ApiResponse<>("SUCESS","List of Students",data);
+        return ResponseEntity.ok(apiResponse);
     }
 
-    @GetMapping("/{registrationNumber}")
-    public Student getStudent(@PathVariable String registrationNumber){
+    @GetMapping("/reg/{registrationNumber}")
+    public StudentResponse getStudent(@PathVariable String registrationNumber){
         return studentService.findByRegistrationNumber(registrationNumber);
+    }
+
+    @GetMapping("/{id}")
+    public StudentResponse getStudent(@PathVariable Long id){
+        return studentService.findById(id);
     }
 
     @PostMapping
