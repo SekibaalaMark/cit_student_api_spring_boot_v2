6ddaaaffe4121cfa6fb9d7e518c2e0399adad029
sekibaalamark <markmajje256@gmail.com>
2026-08-08 10:11:35 +0300

    Add Clean student Response class

---
+++ src/main/java/com/cit/student_api_v2/student/dto/StudentResponse.java
@@ -0,0 +1,31 @@
+package com.cit.student_api_v2.student.dto;
+
+public class StudentResponse {
+    private String name;
+    private String registrationNumber;
+    private Double cgpa;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public Double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(Double cgpa) {
+        this.cgpa = cgpa;
+    }
+}
