dfd9aa8e8d1ed3a323098904360a5ada27e6b9d7
sekibaalamark <markmajje256@gmail.com>
2026-08-10 15:59:55 +0300

    Added error handling on Student Not Found

--- src/main/java/com/cit/student_api_v2/student/service/StudentService.java
+++ src/main/java/com/cit/student_api_v2/student/service/StudentService.java
@@ -3,6 +3,7 @@ package com.cit.student_api_v2.student.service;
 import com.cit.student_api_v2.student.dto.StudentRequest;
 import com.cit.student_api_v2.student.dto.StudentResponse;
 import com.cit.student_api_v2.student.dto.StudentUpdateRequest;
+import com.cit.student_api_v2.student.exception.StudentNotFoundException;
 import com.cit.student_api_v2.student.mapper.StudentMapper;
 import com.cit.student_api_v2.student.model.Student;
 import com.cit.student_api_v2.student.repository.StudentRepository;
@@ -26,12 +27,12 @@ public class StudentService {
 
     }
     public StudentResponse findByRegistrationNumber(String registrationNumber){
-        Student student = studentRepository.findByRegistrationNumber(registrationNumber);
+        Student student = studentRepository.findByRegistrationNumber(registrationNumber).orElseThrow(()-> new StudentNotFoundException("Student with registration Number "+ registrationNumber + " Not Found"));
         return studentMapper.toResponse(student);
     }
 
     public Character grade(Long id){
-        Student student = studentRepository.findById(id).orElseThrow(()-> new RuntimeException("Studnt Not Found"));
+        Student student = studentRepository.findById(id).orElseThrow(()-> new StudentNotFoundException("Student with id: " + id + "Not Found"));
         return studentGrader.grade(student.getCgpa());
     }
 
@@ -60,16 +61,14 @@ public class StudentService {
 
     public StudentResponse findById(Long id){
         Student student = studentRepository.findById(id).
-                orElseThrow(() -> new RuntimeException("Student not found"));
-
+                orElseThrow(() -> new StudentNotFoundException("Student with Id: " + id + " Not found"));
         return studentMapper.toResponse(student);
 
     }
 
-    @Transactional
     public StudentResponse updateStudent(Long id, StudentUpdateRequest updateRequest) {
         Student existingStudent = studentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Student not found"));
+                .orElseThrow(() -> new StudentNotFoundException("Student with id: " + id + "Not Found"));
 
         studentMapper.updateEntityFromRequest(updateRequest , existingStudent);
         Student student = studentRepository.save(existingStudent);
