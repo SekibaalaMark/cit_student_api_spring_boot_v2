c846b330bc8bc40b692aadda6d5c2edee85f2d88
sekibaalamark <markmajje256@gmail.com>
2026-08-10 15:59:29 +0300

    On FindByRegistrationNumber method in student Repository made student optional

--- src/main/java/com/cit/student_api_v2/student/repository/StudentRepository.java
+++ src/main/java/com/cit/student_api_v2/student/repository/StudentRepository.java
@@ -4,10 +4,12 @@ import com.cit.student_api_v2.student.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
-    Student findByRegistrationNumber(String registrationNumber);
+    Optional<Student> findByRegistrationNumber(String registrationNumber);
 
     void deleteByRegistrationNumber(String registrationNumber);
 }
