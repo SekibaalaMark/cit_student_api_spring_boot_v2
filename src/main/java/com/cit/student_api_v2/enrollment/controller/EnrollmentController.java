77596142e1520647f4ccaa268c39fc72bd191380
sekibaalamark <markmajje256@gmail.com>
2026-08-21 15:39:49 +0300

    Create Enrollment Entity controller class with a POST mapping

---
+++ src/main/java/com/cit/student_api_v2/enrollment/controller/EnrollmentController.java
@@ -0,0 +1,31 @@
+package com.cit.student_api_v2.enrollment.controller;
+
+import com.cit.student_api_v2.api.response.ApiResponse;
+import com.cit.student_api_v2.enrollment.dto.EnrollmentRequest;
+import com.cit.student_api_v2.enrollment.dto.EnrollmentResponse;
+import com.cit.student_api_v2.enrollment.model.Enrollment;
+import com.cit.student_api_v2.enrollment.service.EnrollmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/v1/enrollments")
+public class EnrollmentController {
+    private final EnrollmentService enrollmentService;
+
+    public EnrollmentController(EnrollmentService enrollmentService) {
+        this.enrollmentService = enrollmentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<EnrollmentResponse>> addEnrollment(@RequestBody EnrollmentRequest enrollmentRequest){
+        ApiResponse<EnrollmentResponse> apiResponse = enrollmentService.addEnrollment(enrollmentRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+
+    }
+}
