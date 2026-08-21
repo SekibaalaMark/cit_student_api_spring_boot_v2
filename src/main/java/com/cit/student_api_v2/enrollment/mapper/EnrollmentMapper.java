39ba413e17f5cb0255398e86aeea39e167615262
sekibaalamark <markmajje256@gmail.com>
2026-08-21 15:40:32 +0300

    create mapper for enrollment response and request

---
+++ src/main/java/com/cit/student_api_v2/enrollment/mapper/EnrollmentMapper.java
@@ -0,0 +1,21 @@
+package com.cit.student_api_v2.enrollment.mapper;
+
+
+
+import com.cit.student_api_v2.enrollment.dto.EnrollmentResponse;
+import com.cit.student_api_v2.enrollment.model.Enrollment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnrollmentMapper {
+
+    public EnrollmentResponse toResponse(Enrollment enrollment) {
+        return new EnrollmentResponse(
+                enrollment.getId(),
+                enrollment.getStudent().getId(),
+                enrollment.getStudent().getRegistrationNumber(),
+                enrollment.getStudent().getName(),
+                enrollment.getCreatedAt()
+        );
+    }
+}
