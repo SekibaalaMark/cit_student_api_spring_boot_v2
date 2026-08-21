719a1da25d58de6778f279039250c2b65b25c05a
sekibaalamark <markmajje256@gmail.com>
2026-08-21 15:43:27 +0300

    add findByCgpaGreaterThanEqual in student respository

--- src/main/java/com/cit/student_api_v2/student/repository/StudentRepository.java
+++ src/main/java/com/cit/student_api_v2/student/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.cit.student_api_v2.student.repository;
 
 import com.cit.student_api_v2.student.model.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -12,4 +15,6 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
     Optional<Student> findByRegistrationNumber(String registrationNumber);
 
     void deleteByRegistrationNumber(String registrationNumber);
+
+    Page<Student> findByCgpaGreaterThanEqual(double cgpa, Pageable pageable);
 }
