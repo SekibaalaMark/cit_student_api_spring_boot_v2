43ec09cf6579ae1447546012fa8da87921789b29
sekibaalamark <markmajje256@gmail.com>
2026-08-24 08:17:49 +0300

    Add Method to query students using status with their enrollments

--- src/main/java/com/cit/student_api_v2/student/repository/StudentRepository.java
+++ src/main/java/com/cit/student_api_v2/student/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.cit.student_api_v2.student.repository;
 
+import com.cit.student_api_v2.student.model.Status;
 import com.cit.student_api_v2.student.model.Student;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +20,10 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
     void deleteByRegistrationNumber(String registrationNumber);
 
     Page<Student> findByCgpaGreaterThanEqual(double cgpa, Pageable pageable);
+
+
+    @Query("SELECT s from Student s JOIN FETCH s.enrollments WHERE s.status = :status")
+    List<Student> findByStatusWithEnrollment(@Param("status") Status status);
+
+
 }
