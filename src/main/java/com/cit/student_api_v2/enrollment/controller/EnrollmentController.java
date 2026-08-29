4c506779b852b6d5cfb0c9ed056048f4fe8ac846
sekibaalamark <markmajje256@gmail.com>
2026-08-29 10:25:04 +0300

    Add Controller to get all enrollments with pagination and sorting

--- src/main/java/com/cit/student_api_v2/enrollment/controller/EnrollmentController.java
+++ src/main/java/com/cit/student_api_v2/enrollment/controller/EnrollmentController.java
@@ -4,6 +4,7 @@ import com.cit.student_api_v2.api.response.ApiResponse;
 import com.cit.student_api_v2.enrollment.dto.EnrollmentRequest;
 import com.cit.student_api_v2.enrollment.dto.EnrollmentResponse;
 import com.cit.student_api_v2.enrollment.service.EnrollmentService;
+import com.cit.student_api_v2.page.response.PageResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -22,6 +23,21 @@ public class EnrollmentController {
     public ResponseEntity<ApiResponse<EnrollmentResponse>> addEnrollment(@RequestBody EnrollmentRequest enrollmentRequest){
         ApiResponse<EnrollmentResponse> apiResponse = enrollmentService.addEnrollment(enrollmentRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<PageResponse<EnrollmentResponse>>> getAllEnrollments(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int pageSize
+    ){
+        PageResponse<EnrollmentResponse> pageResponse = enrollmentService.getAllEnrollments(page,pageSize);
+        ApiResponse<PageResponse<EnrollmentResponse>> apiResponse = new ApiResponse<>(
+                "SUCCESS",
+                "Page Of Enrollments",
+                pageResponse
+                );
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+
     }
 
     @GetMapping("summary/id/{id}")
