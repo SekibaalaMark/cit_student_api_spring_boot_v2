088ef4bd8d7d69a69728abe572d0dc57ce730a57
sekibaalamark <markmajje256@gmail.com>
2026-08-29 11:21:27 +0300

    Made EnrollmentNotFoundException have a generic message to avoid repeating strings

--- src/main/java/com/cit/student_api_v2/enrollment/exception/EnrollmentNotFoundException.java
+++ src/main/java/com/cit/student_api_v2/enrollment/exception/EnrollmentNotFoundException.java
@@ -2,6 +2,10 @@ package com.cit.student_api_v2.enrollment.exception;
 
 public class EnrollmentNotFoundException extends RuntimeException {
     public EnrollmentNotFoundException(String message){
-        super(message);
+        super(
+                "Enrollment with "+
+                message
+                + " Not Found"
+        );
     }
 }
