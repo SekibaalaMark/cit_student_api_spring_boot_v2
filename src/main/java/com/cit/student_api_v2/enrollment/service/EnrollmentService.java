aebff149a25bfc56f917777ee2fe7dd0a0a0fd81
sekibaalamark <markmajje256@gmail.com>
2026-08-29 08:35:03 +0300

    Add Service method to find Enrollment by Id

--- src/main/java/com/cit/student_api_v2/enrollment/service/EnrollmentService.java
+++ src/main/java/com/cit/student_api_v2/enrollment/service/EnrollmentService.java
@@ -1,20 +1,26 @@
 package com.cit.student_api_v2.enrollment.service;
 
-
 import com.cit.student_api_v2.api.response.ApiResponse;
 import com.cit.student_api_v2.enrollment.dto.EnrollmentRequest;
 import com.cit.student_api_v2.enrollment.dto.EnrollmentResponse;
+import com.cit.student_api_v2.enrollment.exception.EnrollmentNotFoundException;
 import com.cit.student_api_v2.enrollment.facade.EnrollmentFacade;
+import com.cit.student_api_v2.enrollment.mapper.EnrollmentMapper;
 import com.cit.student_api_v2.enrollment.model.Enrollment;
 import com.cit.student_api_v2.enrollment.repository.EnrollmentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EnrollmentService {
     private final EnrollmentFacade enrollmentFacade;
+    private final EnrollmentRepository enrollmentRepository;
+    private final EnrollmentMapper enrollmentMapper;
 
-    public EnrollmentService(EnrollmentRepository enrollmentRepository, EnrollmentFacade enrollmentFacade) {
+    public EnrollmentService(EnrollmentRepository enrollmentRepository, EnrollmentFacade enrollmentFacade, EnrollmentRepository enrollmentRepository1, EnrollmentMapper enrollmentMapper) {
         this.enrollmentFacade = enrollmentFacade;
+        this.enrollmentRepository = enrollmentRepository1;
+        this.enrollmentMapper = enrollmentMapper;
     }
 
     public ApiResponse<EnrollmentResponse> addEnrollment(EnrollmentRequest enrollmentRequest){
@@ -23,6 +29,14 @@ public class EnrollmentService {
                 "Enrollment Added Successfully",
                 enrollmentResponse
         );
+    }
 
+
+    @Transactional(readOnly = true)
+    public ApiResponse<EnrollmentResponse> getEnrollmentById(Long id){
+        Enrollment enrollment = enrollmentRepository.findById(id)
+                .orElseThrow(() -> new EnrollmentNotFoundException("Enrollment with "+ id + " not Found"));
+        EnrollmentResponse enrollmentResponse = enrollmentMapper.toResponse(enrollment);
+        return new ApiResponse<>("SUCCESS","Enrollment item",enrollmentResponse);
     }
 }
