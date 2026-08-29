981bf14816c6ce242695d4f6adc41c69155d441f
sekibaalamark <markmajje256@gmail.com>
2026-08-29 09:08:00 +0300

    Add query to fetch enrollment with the associated student

--- src/main/java/com/cit/student_api_v2/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/cit/student_api_v2/enrollment/repository/EnrollmentRepository.java
@@ -1,10 +1,15 @@
 package com.cit.student_api_v2.enrollment.repository;
 
 import com.cit.student_api_v2.enrollment.model.Enrollment;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
 
+    @Query("SELECT e FROM Enrollment e JOIN FETCH e.student WHERE e.id = :id")
+    Optional<Enrollment> findByIdWithSummary(@Param("id") Long id);
 }
