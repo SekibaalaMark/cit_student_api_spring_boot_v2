93f40e74c292303e762d854d7bb1036dc6f32de4
sekibaalamark <markmajje256@gmail.com>
2026-09-03 23:23:30 +0300

    Introduced Lombok on Enrollment Entity

--- src/main/java/com/cit/student_api_v2/enrollment/model/Enrollment.java
+++ src/main/java/com/cit/student_api_v2/enrollment/model/Enrollment.java
@@ -4,52 +4,47 @@ package com.cit.student_api_v2.enrollment.model;
 import com.cit.student_api_v2.course.model.Course;
 import com.cit.student_api_v2.student.model.Student;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 
+@Data
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "enrollments")
 public class Enrollment {
-
-    public Enrollment() {
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Student student;
 
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "course_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Course course;
 
-
     private LocalDateTime createdAt;
-
 
     @PrePersist
     protected void onCreate(){
         createdAt = LocalDateTime.now();
     }
 
-    public Long getId() {
-        return id;
-    }
-
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
+    public Enrollment(Student student, Course course, LocalDateTime createdAt) {
         this.student = student;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
+        this.course = course;
+        this.createdAt = createdAt;
     }
 }
