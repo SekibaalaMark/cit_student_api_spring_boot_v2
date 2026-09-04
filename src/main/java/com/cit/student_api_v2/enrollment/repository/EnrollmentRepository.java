23b22daac4dcb9108f152113e834f4a4517ebae0
sekibaalamark <markmajje256@gmail.com>
2026-09-04 16:04:18 +0300

    Add method to delete enrollment by ID

--- src/main/java/com/cit/student_api_v2/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/cit/student_api_v2/enrollment/repository/EnrollmentRepository.java
@@ -3,6 +3,7 @@ package com.cit.student_api_v2.enrollment.repository;
 import com.cit.student_api_v2.enrollment.model.Enrollment;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,9 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
 
     @Query("SELECT e FROM Enrollment e JOIN FETCH e.student WHERE e.id = :id")
     Optional<Enrollment> findByIdWithSummary(@Param("id") Long id);
+
+
+    @Modifying
+    @Query("DELETE FROM Enrollment e WHERE e.id = :id")
+    int deleteEnrollmentById(@Param("id") Long id);
 }
